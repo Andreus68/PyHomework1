21163f7cbbb69036191dd88e758c642d10cfd77e
unknown <perecpon@yandex.ru>
2022-11-26 00:47:15 +0300

    holiday

---
+++ task1.py
@@ -0,0 +1,12 @@
+
+
+
+numbers = int(input('Введите число от 1 до 7:   '))
+if numbers < 1 or numbers > 7:
+    print('Не верное число') 
+elif numbers > 5:
+    print('Это выходной')    
+else:
+    print('Это рабочий день') 
+
+
